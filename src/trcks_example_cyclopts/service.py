c1c5421e469a1417c624fa8e824425b544b1b039
Christoph Gietl <christophgietl@users.noreply.github.com>
2026-06-23 08:28:22 +0200

    Refactor trcks_example_cyclopts.service

--- src/trcks_example_cyclopts/service.py
+++ src/trcks_example_cyclopts/service.py
@@ -1,5 +1,6 @@
 import logging
-from typing import TYPE_CHECKING, Final, Generic, Literal, NamedTuple, TypeVar
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Final, Literal
 
 from trcks.oop import TupleWrapper
 
@@ -8,45 +9,59 @@ if TYPE_CHECKING:
 
     from trcks import Result, ResultTuple
 
-type _ReadErrorReason = Literal[
+type _ReadFileErrorReason = Literal[
     "Encoding error in input file",
     "Input file not found",
     "Input path is a directory",
     "Not enough permissions for input file",
 ]
-type _WriteErrorReason = Literal[
+type _WriteFileErrorReason = Literal[
     "Encoding error in output file",
     "Not enough permissions for output file",
     "Output file not found",
     "Output path is a directory",
 ]
-type FileErrorReason = _ReadErrorReason | _WriteErrorReason
-_R_co = TypeVar("_R_co", bound=FileErrorReason, covariant=True)
+type FileErrorReason = _ReadFileErrorReason | _WriteFileErrorReason
 
 _logger: Final = logging.getLogger(__name__)
 
 
-class FileError(NamedTuple, Generic[_R_co]):  # noqa: UP046 # see https://github.com/python/mypy/issues/17623
-    reason: _R_co
+@dataclass(frozen=True, kw_only=True, slots=True)
+class FileError:
+    reason: FileErrorReason
     path: Path | None
 
 
-def _read(input_: Path) -> Result[FileError[_ReadErrorReason], str]:
+@dataclass(frozen=True, kw_only=True, slots=True)
+class _ReadFileError(FileError):
+    reason: _ReadFileErrorReason
+
+
+@dataclass(frozen=True, kw_only=True, slots=True)
+class _WriteFileError(FileError):
+    reason: _WriteFileErrorReason
+
+
+def _read(input_: Path) -> Result[_ReadFileError, str]:
     try:
         with input_.open("r") as f:
             s = f.read()
     except FileNotFoundError:
-        return "failure", FileError(reason="Input file not found", path=input_)
+        return "failure", _ReadFileError(reason="Input file not found", path=input_)
     except (
         IsADirectoryError
     ):  # pragma: no cover # Python for Windows raises a PermissionError instead.
-        return "failure", FileError(reason="Input path is a directory", path=input_)
+        return "failure", _ReadFileError(
+            reason="Input path is a directory", path=input_
+        )
     except PermissionError:
-        return "failure", FileError(
+        return "failure", _ReadFileError(
             reason="Not enough permissions for input file", path=input_
         )
     except ValueError:
-        return "failure", FileError(reason="Encoding error in input file", path=input_)
+        return "failure", _ReadFileError(
+            reason="Encoding error in input file", path=input_
+        )
     else:
         return "success", s
 
@@ -55,9 +70,7 @@ def _transform(s: str) -> str:
     return f"Length: {len(s)}"
 
 
-def _write(
-    s: str, *, output: Path | None
-) -> Result[FileError[_WriteErrorReason], None]:
+def _write(s: str, *, output: Path | None) -> Result[_WriteFileError, None]:
     if output is None:
         print(s)  # noqa: T201 # needed for CLI output
         return "success", None
@@ -66,24 +79,28 @@ def _write(
         with output.open("a") as f:
             f.write(s + "\n")
     except FileNotFoundError:
-        return "failure", FileError(reason="Output file not found", path=output)
+        return "failure", _WriteFileError(reason="Output file not found", path=output)
     except (
         IsADirectoryError
     ):  # pragma: no cover # Python for Windows raises a PermissionError instead.
-        return "failure", FileError(reason="Output path is a directory", path=output)
+        return "failure", _WriteFileError(
+            reason="Output path is a directory", path=output
+        )
     except PermissionError:
-        return "failure", FileError(
+        return "failure", _WriteFileError(
             reason="Not enough permissions for output file", path=output
         )
     except ValueError:
-        return "failure", FileError(reason="Encoding error in output file", path=output)
+        return "failure", _WriteFileError(
+            reason="Encoding error in output file", path=output
+        )
     else:
         return "success", None
 
 
 def read_transform_write(
     inputs: tuple[Path, ...], output: Path | None
-) -> ResultTuple[FileError[FileErrorReason], None]:
+) -> ResultTuple[FileError, None]:
     return (
         TupleWrapper(inputs)
         .map_to_result(_read)
